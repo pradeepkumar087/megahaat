0af9de18131b84e550ec6f1fb17819c31157944b
navaldabral <navaldabral@gmail.com>
2018-01-15 15:18:25 +0530

    About Us
    Changes by naval

---
+++ about-us.php
@@ -0,0 +1,63 @@
+<?php
+	include_once("includes/global.php");
+	include("includes/functions.php");
+	include_once("includes/check_login_mem.php");	
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<title>About Us | MSME Helpline Private Limited </title>
+<meta name="keywords" content="MSME, MSME Helpline, MSME Registration, Bank Loan, Service Tax Registration, Patent, interest free loan, SME Exchange, Online Loan Application in India  " />
+<meta name="description" content="Msmehelpline.com aims to help you to set your enterprise in a very genuine and authentic way. Now easily obtain MSME registrations, Bank loans, interest free loan at Msmehelpline.com . "/>
+<!-- Bootstrap -->
+<link href="css/bootstrap.css" rel="stylesheet"/>
+<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+<![endif]-->
+<!-- Dreamzsop -->
+<link href="css/dreamzsop.css" rel="stylesheet"/>
+<link href="css/font-awesome.min.css" rel="stylesheet">
+<link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
+</head>
+<body>
+<?php include("sidebar-menu.php");?>
+<?php include("header.php");?>
+<div class="internal-container"><!-- internal-container start-->
+<div class="inner-page">
+<div class="container"><h1 class="heading-page">About Us</h1></div>
+</div>
+<div class="container"><!-- container start-->
+<div class="row">
+<div class="col-md-12 col-sm-12 col-xs-12">
+
+<p class="text-justify">One of the major issues and challenges faced by MSME Entrepreneurs is lack of awareness of various schemes available for them from various stakeholders like Government, RBI, Bankers etc. They are also facing the Marketing issues in a big way. MSME Registration is also one of the biggest challenge for them.</p>
+
+<p class="text-justify">To overcome various challenges being faced by MSME Entrepreneurs, we have decided to provide a strong tool in the hands of MSMEs namely MSME Helpline Mobile App as well as portal <a href="<?php echo $site_path; ?>">www.msmehelpline.com.</a> MSME Helpline Mobile App is available for download from “Google Play Store” and “Apple App Store”.</p>
+
+<p class="text-justify">MSME Helpline is an initiative of Indian MSME Helpline Private Limited. Indian MSME Helpline Private Limited is duly registered with Ministry of Corporate Affairs vide Corporate Identification Number  U74999DL2015PTC286119.  The registered office of the company is 1106, New Delhi House, Barakhamba Road, Connaught Place, New Delhi-110001. This MSME Helpline is not operated by Ministry of Micro Small & Medium Enterprises, Government of India. This MSME Helpline is operated by Indian MSME Helpline Private Limited.</p>
+
+<p class="text-justify">MSME Helpline is run by professionals from different fields like Chartered Accountant, Ex Bankers etc.</p>
+
+
+  
+    
+
+ 
+    
+  
+</div>
+</div>
+
+
+</div><!-- container end-->
+</div><!-- internal-container end-->
+<div class="footer"><?php include("footer.php");?></div>
+</body>
+</html>
